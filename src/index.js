aa5b6252689789c4c3410c34ca9f777404e7001e
max0n4ik <max2004.gorohov@yandex.ru>
2023-08-05 01:37:08 +0300

    init: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let arr = n.toString().split("");
+    const arr2 = n.toString().split("");
+    let i = 0;
+    let j = 1;
+    if (arr2[0] == "-") {
+        arr.splice(0, 1);
+        arr2.splice(0, 1);
+        while (arr.length > i) {
+            arr[i] = arr2[arr2.length - j];
+            i++;
+            j++;
+        }
+        return arr.join("");
+    } else {
+        while (arr.length > i) {
+            arr[i] = arr2[arr2.length - j];
+            i++;
+            j++;
+        }
+        return arr.join("");
+    }
+};
